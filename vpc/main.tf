38348c85af84948c4e5fd56ce00dcf9517bed36b
Manu Chandrasekhar <manuchn@amazon.com>
2025-02-05 01:03:06 -0500

    fix: removed vpc ipam pool

--- vpc/main.tf
+++ vpc/main.tf
@@ -4,21 +4,8 @@ resource "aws_vpc_ipam" "this" {
   }
 }
 
-resource "aws_vpc_ipam_pool" "this" {
-  address_family = "ipv4"
-  ipam_scope_id  = aws_vpc_ipam.this.private_default_scope_id
-  locale         = var.region
-}
 
-resource "aws_vpc_ipam_pool_cidr" "this" {
-  ipam_pool_id = aws_vpc_ipam_pool.this.id
-  cidr         = var.cidr_block
-}
 
 resource "aws_vpc" "this" {
-  ipv4_ipam_pool_id   = aws_vpc_ipam_pool.this.id
-  ipv4_netmask_length = 28
-  depends_on = [
-    aws_vpc_ipam_pool_cidr.this
-  ]
+  cidr_block=var.cidr_block
 }
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,7 +2,3 @@ output "vpc_id" {
   value       = aws_vpc.this.id
   description = "VPC id from network stack"
 }
-output "ipam_pool_id" {
-  value       = aws_vpc_ipam_pool.this.id
-  description = "IPAM pool id"
-}
