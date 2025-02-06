2bc2d3f022fb3c186749e1270dc5cd13f81266b6
Manu Chandrasekhar <manuchn@amazon.com>
2025-02-06 18:07:58 -0500

    updated outputs with dummary value

--- outputs.tfstack.hcl
+++ outputs.tfstack.hcl
@@ -2,3 +2,8 @@ output "vpc_id"{
     type=string
     value=component.vpc.vpc_id
 }
+
+output "test_out"{
+    type=string
+    value=component.vpc.test_out
+}
--- vpc/outputs.tf
+++ vpc/outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value       = aws_vpc.this.id
   description = "VPC id from network stack"
 }
+
+output "test_out" {
+  value       = "test_manu"
+  description = "random value"
+}
